5bfa8e661ea3f805dbcbe871617dc563741094a4
jfo <jfortune@mereo.fr>
2021-10-22 17:10:30 +0200

    begin debug class

---
+++ PhiloEngine/src/DebugInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace PhiloEngine.src
+{
+    public class DebugInfo
+    {
+        private string _msg;
+        public string Msg { get => _msg; set => _msg = value; }
+        public DebugInfo(string msg)
+        {
+            _msg = msg;
+        } 
+
+        public void PrintDebug()
+        {
+            Debug.WriteLine(_msg);
+        }
+    }
+}
